43e0c8d437b10fd37e44f0b04fd57fac72f8ae0d
Anshun-raj <60781096+Anshun-raj@users.noreply.github.com>
2022-04-26 13:10:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+function edit1() {
+    var x = document.getElementById("homeText");
+    var a = document.getElementById("textImg");
+    var p = document.getElementById("shoeImg");
+    var h = document.getElementById("home");
+    h.style.backgroundImage = "url('home-bg-1.jpg')";
+    p.src = "home-shoe-1.png";
+    a.src = "home-text-1.png";
+    x.innerText = "Sparx Red Shoes";
+}
+
+function edit2() {
+    var y = document.getElementById("homeText");
+    var b = document.getElementById("textImg");
+    var q = document.getElementById("shoeImg");
+    var h = document.getElementById("home");
+    h.style.backgroundImage = "url('home-bg-2.jpg')";
+    q.src = "home-shoe-2.png";
+    b.src = "home-text-2.png";
+    y.innerText = "Sparx Blue Shoes";
+}
+
+function edit3() {
+    var z = document.getElementById("homeText");
+    var c = document.getElementById("textImg");
+    var r = document.getElementById("shoeImg");
+    var h = document.getElementById("home");
+    h.style.backgroundImage = "url('home-bg-3.jpg')";
+    r.src = "home-shoe-3.png";
+    c.src = "home-text-3.png";
+    z.innerText = "Sparx Orange Shoes";
+}
+
+function changeImg1() {
+    var a = document.getElementById("fb1");
+    a.src = "f-img-1.1.png";
+}
+
+function changeImg2() {
+    var a = document.getElementById("fb1");
+    a.src = "f-img-1.2.png";
+}
+
+function changeImg3() {
+    var a = document.getElementById("fb1");
+    a.src = "f-img-1.3.png";
+}
+
+function changeImg4() {
+    var a = document.getElementById("fb1");
+    a.src = "f-img-1.4.png";
+}
+
+function changeImg5() {
+    var a = document.getElementById("fb2");
+    a.src = "f-img-2.1.png";
+}
+
+function changeImg6() {
+    var a = document.getElementById("fb2");
+    a.src = "f-img-2.2.png";
+}
+
+function changeImg7() {
+    var a = document.getElementById("fb2");
+    a.src = "f-img-2.3.png";
+}
+
+function changeImg8() {
+    var a = document.getElementById("fb2");
+    a.src = "f-img-2.4.png";
+}
+
+function changeImg9() {
+    var a = document.getElementById("fb3");
+    a.src = "f-img-3.1.png";
+}
+
+function changeImg10() {
+    var a = document.getElementById("fb3");
+    a.src = "f-img-3.2.png";
+}
+
+function changeImg11() {
+    var a = document.getElementById("fb3");
+    a.src = "f-img-3.3.png";
+}
+
+function changeImg12() {
+    var a = document.getElementById("fb3");
+    a.src = "f-img-3.4.png";
+}
